63d1d9a372f551a416c1860d2c2b7636b9b29e94
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2024-11-23 23:14:55 +0500

    add just push method

---
+++ ES6-array-methods/es6-array-methods.js
@@ -0,0 +1,41 @@
+//? Array
+
+//? Simple array example
+
+    // let numbers = [10,20,30,40,50,60]
+
+    //?Print all elements of arr
+    // console.log(`All elements of array is  ${numbers} `)
+    //! output: (6) [10, 20, 30, 40, 50, 60]
+    
+    //? Print only second element of arr
+    // console.log(`3rd element of array is ${numbers[2]} `)
+    //! output: 30
+
+    //? sum all elements of array
+    // let sum = 0;
+    // for(var i = 0; i < numbers.length; i++){
+    //     sum = sum + numbers[i]
+    // }
+    // console.log(`The sum of all elements in array ${sum}`)
+    //! output: (6) [10, 20, 30, 40, 50, 60]
+
+    //? ARRAY METHODS
+
+    //! push,pop,shift,inshift,concat,slice,splice,for each,map,filter,reduce,find,findIndex,every,some,sort,reverse,includes,indexOf,join
+
+    //? 1. Push:
+
+    // Def: add one or more elements to last of array.
+
+    // Example:
+    var names = ['ali','khan']
+    console.log(`before : ${names}`);
+    // output: before : ali,khan
+    
+    names.push('hamza','yahya')
+    console.log(`after: ${names}`)
+    // output: after: ali,khan,hamza,yahya
+
+
+
